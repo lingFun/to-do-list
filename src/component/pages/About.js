096b7fa6b1295ad756c8bca3a1e9f87df4d3005b
lingFun <49457941+lingFun@users.noreply.github.com>
2020-07-14 10:17:08 -0400

    First commit

---
+++ src/component/pages/About.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function About() {
+    return (
+        <React.Fragment>
+            <h1>
+                About
+            </h1>
+            <p>
+                This is the TodoList app v1.0.0.
+                Source: <a href="https://www.youtube.com/watch?v=sBws8MSXN7A">React JS Crash Course on Youtube</a>.
+                06/05/2020
+            </p>
+        </React.Fragment>
+    )
+}
